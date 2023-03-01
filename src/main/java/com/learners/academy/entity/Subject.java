c017ebf33687d16e758084e7f699802161efe95f
rlopezb <rlopezb@gmail.com>
2023-03-01 18:31:49 +0100

    Initial commit

---
+++ src/main/java/com/learners/academy/entity/Subject.java
@@ -0,0 +1,20 @@
+package com.learners.academy.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Subject {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+}
